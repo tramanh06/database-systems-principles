058a95cabdd5d609a6f1f785c1dca14dcb01392e
Tram Anh <tramanh06@gmail.com>
2015-10-29 20:29:03 +0800

    Change column name to prevent keyword key

--- import_relations.sql
+++ import_relations.sql
@@ -7,20 +7,20 @@ drop table if exists R, S;
 
 create table R
 	(
-		key int,
-		value varchar(255)
+		id int,
+		name varchar(255)
 	);
 
 create table S
 	(
-		key int,
-		value varchar(255)
+		id int,
+		name varchar(255)
 	);
 
-LOAD DATA INFILE "/Users/TramAnh/Dropbox/NTU/Year 4 Sem 1/CZ4031 Database System Principles/Project/Assignment 2/Project2Code/RelR.txt"
+LOAD DATA INFILE "/Users/TramAnh/Dropbox/NTU/Year 4 Sem 1/CZ4031 Database System Principles/Project/Assignment 2/NetBean Project/RelR.txt"
 INTO TABLE R 
 FIELDS TERMINATED BY ' ' LINES TERMINATED BY '\n';
 
-LOAD DATA INFILE "/Users/TramAnh/Dropbox/NTU/Year 4 Sem 1/CZ4031 Database System Principles/Project/Assignment 2/Project2Code/RelS.txt"
+LOAD DATA INFILE "/Users/TramAnh/Dropbox/NTU/Year 4 Sem 1/CZ4031 Database System Principles/Project/Assignment 2/NetBean Project/RelS.txt"
 INTO TABLE S 
 FIELDS TERMINATED BY ' ' LINES TERMINATED BY '\n';
